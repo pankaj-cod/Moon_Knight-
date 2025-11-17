df30907b32b3e5f70c30a0303560063fa6a785d6
Pankaj Upadhyay <pankaj@Pankajs-MacBook-Pro.local>
2025-11-17 17:38:23 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
   const imgRef = useRef(null);
   const fileInputRef = useRef(null);
 
-  const API_URL = process.env.VITE_APP_API_URL || 'http://localhost:5001/api';
+  const API_URL = import.meta.env.VITE_APP_API_URL ;
 
   useEffect(() => {
     const token = localStorage.getItem('token');
